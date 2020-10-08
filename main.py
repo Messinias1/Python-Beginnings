0f966e7ae69f65890f56149e6964151a001471ea
Carl Kakisis <Messinias1@gmail.com>
2020-10-08 17:39:14 -0400

    variable play

---
+++ main.py
@@ -0,0 +1,10 @@
+name = "John"
+age = "35"
+
+print("There was once a man named " + name + ", ")
+print("he was " + age + " years old")
+
+name = "Carl"
+newAge = 25
+isAdult = True
+print(name + " is " + format(int(newAge)) + ".")
